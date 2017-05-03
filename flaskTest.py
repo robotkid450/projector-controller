4eb4ba4d966d282d7845c44adc6573ab47fa5899
Josh <josh@mediaserver.robotkid450.com>
2017-05-03 13:11:53 -0500

    moving

--- flaskTest.py
+++ flaskTest.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 
-from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def show_login():
-    return render_template('index.html')
+	if request.method == 'GET':
+		print("nom")
+		return render_template('projector control.html')
+	
+	elif request.method == 'POST':
+		action = request.form['submit']
+		
+		if action == 'powerOn':
+			return "power on"
+		
+    
+@app.route('/login', methods=['POST'])
+def process_login():
+	_uname = request.form['username']
+	_password = reqest.form['password']
+	
+@app.route('/lampHours')
+def show_lamp_hours():
+	return "lamp hours"
     
     
 if __name__ == "__main__":
