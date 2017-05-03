ec0389347a9e83bde2efebb2d569bceee56b6331
Josh Smith <robotkid450@gmail.com>
2017-05-03 16:39:43 +0000

    made test template

--- flaskTest.py
+++ flaskTest.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def show_login():
-    return 'Login'
+    return render_template('index.html')
     
     
 if __name__ == "__main__":
