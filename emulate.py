259ebf1f746e478833b2a779e7c32fedf6d86658
Josh Smith <robotkid450@gmail.com>
2017-05-04 17:46:00 +0000

    added input to emulate.py

--- emulate.py
+++ emulate.py
@@ -20,26 +20,53 @@ def _readline(serialCon):
 
 
 power = 0
+vidin = 1
 lamp1 = '57'
 lamp2 = '999'
 
 a = serial.Serial(port=sys.argv[1], baudrate=19200)
 while True:
+    time.sleep(.1)
     if a.in_waiting > 0:
         line = _readline(a)
         print('line', line)
+        
+        # change status from power on
         if line == 'C00':
             print('power on')
-            global power
+            #global power
             power = 1
             a.write('\x06\r'.encode())
     
+        # change status from power off
         if line == 'C01':
             print('power off')
-            global power
+            #global power
             power = 0
             a.write('\x06\r'.encode())
-    
+            
+        # set input 1
+        if line == 'C05':
+            print('input 1')
+            vidin = 1
+            a.write('\x06\r'.encode())
+            
+        # set input 2
+        if line == 'C06':
+            print('input 2')
+            vidin = 2
+            a.write('\x06\r'.encode())
+            
+        # set input 3
+        if line == 'C07':
+            print('input 3')
+            vidin = 3
+            a.write('\x06\r'.encode())
+            
+        
+            
+        
+        #gen stat
         if line == 'CR0':
             print(power)
             if power == 0:
@@ -48,7 +75,11 @@ while True:
             else:
                 a.write('00\r'.encode())
                 print('power on send')
-                
+        
+        if line == 'CR1':
+            a.write(str(vidin).encode())
+
+        # lamp Hours
         if line == 'CR3':
             lhour = lamp1 + ' ' + lamp2 + '\r'
             a.write(lhour.encode())
