6b6bb41ea87647e37c6ef84f09ee3ba1ae22d039
Josh Smith <robotkid450@gmail.com>
2017-05-04 16:54:37 +0000

    added status reporting and lamp hour reporting

--- main.py
+++ main.py
@@ -6,26 +6,73 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+def render_main(projector1, projector2):
+	genstatP1 = projector1.getStatusGeneral()
+	genstatP2 = projector2.getStatusGeneral()
+	lamphourL1P1, lamphourL2P1 = projector1.getLampHour()
+	#lamphour = projector1.getLampHour()
+	return render_template('projector_control.html', statusP1 = genstatP1, statusP2 = genstatP2, l1hoursP1 = lamphourL1P1, l2hoursP1 = lamphourL2P1)
+
 @app.route('/', methods=['POST', 'GET'])
 def main_control():
 	#p1 = Sanyo.projector(port='/dev/ttyS0')
 	#p2 = Sanyo.projector(port='/dev/ttyS1')
-	p1 = Sanyo.projector(port='/dev/ttyUSB0')
-	p2 = Sanyo.projector(port='/dev/ttyUSB1')
-	statusP1 = p1.getStatusGeneral()
-	#status = 'a'
+	#p1 = Sanyo.projector(port='/dev/ttyUSB0')
+	#p2 = Sanyo.projector(port='/dev/ttyUSB1')
+	p1 = Sanyo.projector(port='/dev/pts/6')
+	p2 = Sanyo.projector(port='/dev/pts/8')
+
 	if request.method == 'GET':
-		print("nom")
-		return render_template('projector_control.html', statusP1 = statusP1)
+		return render_main(p1, p2)
 
 	elif request.method == 'POST':
-		print(request.form)
 		action = request.form['submit']
-		print(action)
-		if action == 'powerOn':
-			print("power on")
+		# projector 1
+		if action == 'powerOnP1':
+			print("power on P1")
+			p1.powerOn()
+			
+		elif action == 'powerOffP1':
+			print("power Off P1")
+			p1.powerOff()
+			
+		elif action == 'Input1P1':
+			print("input 1 P1")
+			p1.setInput(1)
+		
+		elif action == 'Input2P1':
+			print("input 2 P1")
+			p1.setInput(2)
+		
+		elif action == 'Input3P1':
+			print("input 2 P1")
+			p1.setInput(3)
+		
+		# projector 2
+		if action == 'powerOnP2':
+			print("power on P2")
+			p2.powerOn()
+			
+		elif action == 'powerOffP2':
+			print("power Off P2")
+			p2.powerOff()
+			
+		elif action == 'Input1P2':
+			print("input 1 P2")
+			p2.setInput(1)
+		
+		elif action == 'Input2P2':
+			print("input 2 P2")
+			p2.setInput(2)
+		
+		elif action == 'Input3P2':
+			print("input 2 P2")
+			p2.setInput(3)
 
-		return render_template('projector_control.html')
+
+		return render_main(p1, p2)
+		
 
 @app.route('/confirm', methods=['POST','GET'])
 def show_confirmation():
