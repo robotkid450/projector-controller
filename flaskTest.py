903708d4ecf8e5c08613b710f6a17a11d832628b
Josh Smith <robotkid450@gmail.com>
2017-05-03 21:21:18 -0500

    working post setup

--- flaskTest.py
+++ flaskTest.py
@@ -6,27 +6,40 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
-def show_login():
+def main_control():
 	if request.method == 'GET':
 		print("nom")
-		return render_template('projector control.html')
-	
+		return render_template('projector_control.html')
+
 	elif request.method == 'POST':
+		print(request.form)
 		action = request.form['submit']
-		
+		print(action)
 		if action == 'powerOn':
-			return "power on"
-		
-    
+			print("power on")
+
+		return render_template('projector control.html')
+
+@app.route('/confirm', methods=['POST','GET'])
+def show_confirmation():
+	if request.method == 'POST':
+		pass
+
+	elif request.method == 'GET':
+		return render_template()
+
+
+
+
 @app.route('/login', methods=['POST'])
 def process_login():
 	_uname = request.form['username']
 	_password = reqest.form['password']
-	
+
 @app.route('/lampHours')
 def show_lamp_hours():
 	return "lamp hours"
-    
-    
+
+
 if __name__ == "__main__":
     app.run()
